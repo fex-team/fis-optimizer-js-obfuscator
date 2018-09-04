905b5d1420e962f7e3a7404f096f54a9123b8f86
zhangjun08 <zhangjun08@baidu.com>
2018-09-04 18:49:21 +0800

    0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # fis-optimizer-js-obfuscator
-javascript-obfuscator的fis插件
+[javascript-obfuscator](https://github.com/javascript-obfuscator/javascript-obfuscator)的fis插件。
+
+## 使用
+
+```
+npm i -g fis-optimizer-js-obfuscator
+```
+
+## 配置
+
+在项目配置文件（默认fis-conf.js）配置
+
+```javascript
+fis.match('your/js/path', {
+  optimizer: fis.plugin('js-obfuscator', option)
+})
+```
+
+`option`是[javascript-obfuscator的配置](https://github.com/javascript-obfuscator/javascript-obfuscator#options)，也可以使用`fis-optimizer-js-obfuscator`内置的配置，如：
+
+```javascript
+fis.match('your/js/path', {
+  optimizer: fis.plugin('js-obfuscator', {obfuscatorLeval: 'high'})
+});
+
+fis.match('your/js/path', {
+  optimizer: fis.plugin('js-obfuscator', {obfuscatorLeval: 'medium'})
+});
+
+fis.match('your/js/path', {
+  optimizer: fis.plugin('js-obfuscator', {obfuscatorLeval: 'low'})
+});
+```
+
+`high`、`medium`、`low`分别对应[javascript-obfuscator的推荐配置](https://github.com/javascript-obfuscator/javascript-obfuscator#high-obfuscation-low-performance)
---
+++ index.js
@@ -0,0 +1,81 @@
+/**
+ * @file fis-optimizer-js-obfuscator
+ * @author jun_zhang2011@qq.com
+ */
+
+var JavaScriptObfuscator = require('javascript-obfuscator');
+
+module.exports = function(content, file, conf) {
+  console.log(conf);
+  if (conf.obfuscatorLeval) {
+    if (conf.obfuscatorLeval === 'high') {  // High obfuscation, low performance
+      conf = {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 1,
+        deadCodeInjection: true,
+        deadCodeInjectionThreshold: 1,
+        debugProtection: true,
+        debugProtectionInterval: true,
+        disableConsoleOutput: true,
+        identifierNamesGenerator: 'hexadecimal',
+        log: false,
+        renameGlobals: false,
+        rotateStringArray: true,
+        selfDefending: true,
+        stringArray: true,
+        stringArrayEncoding: 'rc4',
+        stringArrayThreshold: 1,
+        transformObjectKeys: true,
+        unicodeEscapeSequence: false
+      };
+    } else if (conf.obfuscatorLeval === 'medium') { // Medium obfuscation, optimal performance
+      conf = {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 0.75,
+        deadCodeInjection: true,
+        deadCodeInjectionThreshold: 0.4,
+        debugProtection: false,
+        debugProtectionInterval: false,
+        disableConsoleOutput: true,
+        identifierNamesGenerator: 'hexadecimal',
+        log: false,
+        renameGlobals: false,
+        rotateStringArray: true,
+        selfDefending: true,
+        stringArray: true,
+        stringArrayEncoding: 'base64',
+        stringArrayThreshold: 0.75,
+        transformObjectKeys: true,
+        unicodeEscapeSequence: false
+      };
+    } else if (conf.obfuscatorLeval === 'low') {  // Low obfuscation, High performance
+      conf = {
+        compact: true,
+        controlFlowFlattening: false,
+        deadCodeInjection: false,
+        debugProtection: false,
+        debugProtectionInterval: false,
+        disableConsoleOutput: true,
+        identifierNamesGenerator: 'hexadecimal',
+        log: false,
+        renameGlobals: false,
+        rotateStringArray: true,
+        selfDefending: true,
+        stringArray: true,
+        stringArrayEncoding: false,
+        stringArrayThreshold: 0.75,
+        unicodeEscapeSequence: false
+      };
+    }
+  }
+  try {
+    var obfuscationResult = JavaScriptObfuscator.obfuscate(content, conf);
+    content = obfuscationResult.getObfuscatedCode();
+  } catch(e) {
+    fis.log.warning('Got Error ' + e.message + ' while obfuscator ' + file.subpath);
+    fis.log.debug(e.stack);
+  }
+  return content;
+};
